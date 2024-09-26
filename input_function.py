f1637f56e3042f6b08e1b8cb3a8a2253bd0f3bd1
Dipayan <dipayan0070@gmail.com>
2024-09-26 20:09:22 +0530

    Create input_function.py

---
+++ input_function.py
@@ -0,0 +1,11 @@
+#integer type input
+name=str(input("enter your name: "))
+print("welcome",name)
+
+#Interger type input
+num=int(input("enter a num: "))
+print("the num is ",num)
+
+#floating type input
+val=float(input("enter a num: "))
+print("the num is",val)
