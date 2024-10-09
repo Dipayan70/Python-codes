ab372010fc7f6b80ea1ebdd3dd92d8455038a64b
Dipayan <dipayan0070@gmail.com>
2024-10-09 21:23:07 +0530

    Update python_all_function.py

--- python_all_function.py
+++ python_all_function.py
@@ -2,8 +2,13 @@
 
 s="i am a boy"
 print(s.endswith("er"))#returns true if string ends with substr
+
 print(s.capitalize())#caapitalizes 1st character
+
 print(s.replace("o","a"))#replaces all occurrences of old with new
+
 print(s.replace("boy","man"))
+
 print(s.find("o"))#returns 1st index of 1st occurrenc
+
 print(s.count("a"))#counts the occurrence of substr in string
